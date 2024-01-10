eff0fd2a002d72794a3693a03383b1a4809ebb18
wafa alraeai <wafaalraee9@gmail.com>
2024-01-10 20:05:51 +0100

    reg-form

--- model/doctorModel.js
+++ model/doctorModel.js
@@ -1,25 +1,52 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const doctorSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+    minLength: 3
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  specialization: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  experience: {
+    type: Number,
+    required: true
+  },
+  feePerConsultation: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
 
-     firstname:{
-        type : String,
-        required : true,
-        minLength:3
-    },
-     lastname:{
-        type : String,
-        required : true
-     },
-     email:{
-        type : String,
-        required : true
-     },
-     password:{
-      type: String,
-      required :true 
-     }
-    
-     }, {timestamps: true})
-        const Doctor = mongoose.model('Doctor', doctorSchema)
-        module.exports = Doctor;
+const Doctor = mongoose.model('Doctor', doctorSchema);
+module.exports = Doctor;
