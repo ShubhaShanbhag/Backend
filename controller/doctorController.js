a4580d1ff647d6b139aaea1084171fb2393103d5
Wesam Shujaa <w.shujaa@presspage.com>
2024-01-10 11:10:40 +0100

    login reg

--- controller/doctorController.js
+++ controller/doctorController.js
@@ -6,13 +6,14 @@ const jwt = require('jsonwebtoken')
 // doctor register
 
     const  doctorRegister = async (req, res) => {
-    const { firstname, lastname, password } = req.body;
-    const doctor = await doctorModel.findOne({ email: req.body.email});
+      console.log(req.body);
+    const { firstname, lastname, password, email } = req.body;
+    const doctor = await doctorModel.findOne({ email: email});
     if (doctor) {
       return res.status(400).json({ message: "Doctor name already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newDoctor = new doctorModel({ firstname, lastname , password: hashedPassword });
+    const newDoctor = new doctorModel({ firstname, lastname, email, password: hashedPassword });
     await newDoctor.save();
     res.json({ message: "Doctor registered successfully" });
   };
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const route = require('./config/route')
-const cors =require ('cors')
+const cors = require('cors')
 require('./config/mongoose')
 require('dotenv').config()
 const app = express()
 var cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+    origin: [3000, "http://localhost:3000"],
+    methods: ["GET", "POST", "UPDATE", "DELETE"],
+    preflightContinue: false,
+    optionsSursccessStatus: 204,
+    credentials:true
+}))
+
 app.use(route)
 
 app.listen(4500,()=>{
