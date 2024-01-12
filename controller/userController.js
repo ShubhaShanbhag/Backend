7535b5c1eee8cfccb03dd5986f4b9bfb1788fc1c
wafa alraeai <wafaalraee9@gmail.com>
2024-01-12 10:09:41 +0100

    bookappointment

--- controller/userController.js
+++ controller/userController.js
@@ -125,12 +125,27 @@ const  userRegister = async (req, res) => {
 //     });
 //   }
 //   };
-  
-  
-  module.exports = {
-    userRegister,
-    userLogin,
-    getAllDocotrsController,
+const getDoctorByIdController = async (req, res) => {
+  try {
+    const doctor = await DoctorModel.findById(req.params.id);
+    if (!doctor) {
+      return res.status(404).json({ message: 'Doctor not found' });
+    }
+
+    // Assuming you want to send specific properties of the doctor, adjust as needed
+    const { _id, firstname, lastname, phoneNumber, address } = doctor;
+    res.json({ _id, firstname, lastname, phoneNumber, address });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+module.exports = {
+  userRegister,
+  userLogin,
+  getAllDocotrsController,
+  getDoctorByIdController,
     // bookAppointmnetController,
     // bookingAvailabilityController
 
